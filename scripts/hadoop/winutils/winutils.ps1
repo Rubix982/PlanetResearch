623d6f7ef6cc545f529778abd20d54c1adf1e96d
Saif Ul Islam <saif.islam@astera.com>
2023-07-23 00:14:53 +0300

    ♻ refactor(winutils, logging): Extract logging functions, move to external file
    Signed-off-by: Saif Ul Islam <saif.islam@astera.com>

--- scripts/hadoop/winutils/winutils.ps1
+++ scripts/hadoop/winutils/winutils.ps1
@@ -2,35 +2,7 @@
 $DebugPreference = "Continue"
 $VerbosePreference = "Continue"
 
-# Define function to return timestamp
-function Get-Timestamp
-{
-    return "$( Get-Date -Format 'yyyy-MM-dd HH:mm:ss' )"
-}
-
-# Define function to write progess messages with timestamp
-function Write-TimeStampedProgressMessage($message)
-{
-    Write-Progress -Activity "winutils.ps1" -Status $message
-}
-
-# Define function to write warning messages with timestamp
-function Write-TimeStampedWarningMessage($message)
-{
-    Write-Warning "$( Get-Timestamp ) - $message"
-}
-
-# Define function to write error messages with timestamp
-function Write-TimeStampedErrorMessage($message)
-{
-    Write-Error "$( Get-Timestamp ) - $message"
-}
-
-# Define function to write debug messages with timestamp
-function Write-TimeStampedDebugMessage($message)
-{
-    Write-Debug "$( Get-Timestamp ) - $message"
-}
+Import-Module -Name (Join-Path (Get-Location).Path "\scripts\helpers\logs\logging.ps1") -Force
 
 # Greet user
 Write-TimeStampedDebugMessage "Starting winutils.ps1"
